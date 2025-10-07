182295d8c88b920618a621f9818d5c49c10ab9d1
JigarDavda <37215768+JigarDavda@users.noreply.github.com>
2025-10-07 15:45:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const video = document.querySelector('video');
+    const video = document.querySelector('.layout');
     const buttons = document.querySelectorAll('.button-bar button');
     buttons.forEach(btn => {
         btn.addEventListener('click', function() {
+            // Remove 'selected' from all buttons
+            buttons.forEach(b => b.classList.remove('selected'));
+            // Add 'selected' to the clicked button
+            btn.classList.add('selected');
             const angle = btn.getAttribute('data-angle');
             console.log("angel--", angle)
             video.style.transform = `rotate(${angle}deg)`;
